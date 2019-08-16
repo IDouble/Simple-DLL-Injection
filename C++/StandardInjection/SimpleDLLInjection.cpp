cf1883da85bfcde4e42383ad29f6d3fef1488179
AYIDouble <alpaypython@gmail.com>
2019-08-16 10:10:41 +0200

    🔁 Refactoring

--- C++/StandardInjection/SimpleDLLInjection.cpp
+++ C++/StandardInjection/SimpleDLLInjection.cpp
@@ -18,21 +18,15 @@ int main()
 	GetWindowThreadProcessId(hwnd, &procID); // Getting our Process ID, as an ex. like 000027AC
 	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID); // Opening the Process with All Access
 
-	// Allocate memory for the dllpath in the target process
-	// length of the path string + null terminator
-	LPVOID pDllPath = VirtualAllocEx(handle, 0, strlen(DllPath) + 1,
-		MEM_COMMIT, PAGE_READWRITE);
+	// Allocate memory for the dllpath in the target process, length of the path string + null terminator
+	LPVOID pDllPath = VirtualAllocEx(handle, 0, strlen(DllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
 
-	// Write the path to the address of the memory we just allocated
-	// in the target process
-	WriteProcessMemory(handle, pDllPath, (LPVOID)DllPath,
-		strlen(DllPath) + 1, 0);
+	// Write the path to the address of the memory we just allocated in the target process
+	WriteProcessMemory(handle, pDllPath, (LPVOID)DllPath, strlen(DllPath) + 1, 0);
 
-	// Create a Remote Thread in the target process which
-	// calls LoadLibraryA as our dllpath as an argument -> program loads our dll
-	HANDLE hLoadThread = CreateRemoteThread(handle, 0, 0,
-		(LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"),
-			"LoadLibraryA"), pDllPath, 0, 0);
+	// Create a Remote Thread in the target process which calls LoadLibraryA as our dllpath as an argument -> program loads our dll
+	HANDLE hLoadThread = CreateRemoteThread(handle, 0, 0, 
+	(LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), pDllPath, 0, 0);
 
 	// Wait for the execution of our loader thread to finish
 	WaitForSingleObject(hLoadThread, INFINITE);
@@ -40,8 +34,7 @@ int main()
 	cout << "Dll path allocated at: " << hex << pDllPath << endl;
 	cin.get();
 
-	// Free the memory allocated for our dll path
-	VirtualFreeEx(handle, pDllPath, strlen(DllPath) + 1, MEM_RELEASE);
+	VirtualFreeEx(handle, pDllPath, strlen(DllPath) + 1, MEM_RELEASE); // Free the memory allocated for our dll path
 
 	return 0;
 }
